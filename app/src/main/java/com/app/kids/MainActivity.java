5e712c429f3a81cf532c9e9a7af6d879b8e3ff0a
Kishan Viramgama <viramgama.kishan@gmail.com>
2019-12-10 23:23:36 +0530

    Add old code

---
+++ app/src/main/java/com/app/kids/MainActivity.java
@@ -0,0 +1,71 @@
+package com.app.kids;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.app.drawing.Util.CanvasView;
+import com.app.kids.drawing.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Toolbar toolbar;
+    private CanvasView customCanvas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(getResources().getString(R.string.app_name));
+        setSupportActionBar(toolbar);
+
+        customCanvas = findViewById(R.id.canvas_drawing);
+        ImageView imageView = findViewById(R.id.imageView);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image);
+        imageView.setImageBitmap(bitmap);
+
+        customCanvas.paintColor(getResources().getColor(R.color.colorAccent));
+        customCanvas.paintStork(20);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.ic_drawing:
+                customCanvas.paintColor(getResources().getColor(R.color.colorAccent));
+                break;
+
+            case R.id.ic_eraser:
+                customCanvas.paintColor(getResources().getColor(R.color.white));
+                break;
+
+            case R.id.ic_restart:
+                customCanvas.clear();
+                break;
+
+            default:
+                break;
+        }
+
+        return true;
+    }
+
+}
